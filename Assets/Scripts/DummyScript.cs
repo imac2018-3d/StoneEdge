7407c9ad04894e8ec280a9eba4c0b1ea8ff1829b
Yoan Lecoq <yoanlecoq.io@gmail.com>
2017-10-07 23:56:44 +0200

    Add DummyScript

---
+++ Assets/Scripts/DummyScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyScript : MonoBehaviour {
+
+	void Start () {
+		Debug.Log ("hello world!");
+	}
+}
