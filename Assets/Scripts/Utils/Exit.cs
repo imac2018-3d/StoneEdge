7791cf944b244cde5239c01b457a67975f460bd1
Yoan Lecoq <yoanlecoq.io@gmail.com>
2018-01-03 09:24:43 +0100

    Improve Exit API

--- Assets/Scripts/Utils/Exit.cs
+++ Assets/Scripts/Utils/Exit.cs
@@ -1,23 +1,34 @@
-﻿namespace Utils {
+﻿using UnityEngine;
+using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Utils {
 	/// <summary>
 	/// A convenience static class for exitting the game quickly, both in the Editor and the final game.
 	/// </summary>
 	public static class Exit {
+		/// <summary>
+		/// Exit the game (in the Editor, exits Play Mode).
+		/// </summary>
 		// NOTE: I would have named this method Exit() instead, but C# won't allow me.
-		public static void Now() {
-#if UNITY_EDITOR
-			UnityEditor.EditorApplication.isPlaying = false;
-#endif
-			UnityEngine.Application.Quit ();
-		}
-		public static void If(bool doIt, string reason="") {
-			if (doIt) {
-				if (!string.IsNullOrEmpty (reason)) {
-					// NOTE: Don't throw an exception, otherwise we won't reach the Exit.Now() line.
-					UnityEngine.Debug.LogError("Exitting: " + reason);
-				}
-				Exit.Now ();
+		public static void Now(string reason="") {
+			if (!string.IsNullOrEmpty (reason)) {
+				// NOTE: Don't throw an exception, otherwise we won't reach the Exit.Now() line.
+				Debug.LogError("Exitting: " + reason + Environment.NewLine + Environment.StackTrace);
 			}
+#if UNITY_EDITOR
+			EditorApplication.isPlaying = false;
+#endif
+			Application.Quit ();
+		}
+		/// <summary>
+		/// Exit if a condition is met.
+		/// </summary>
+		public static void If(bool doIt, string reason="") {
+			if (doIt)
+				Exit.Now (reason);
 		}
 	}
 }
