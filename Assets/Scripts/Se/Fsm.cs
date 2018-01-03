4e56566e495e0e031c03d0f8b303d91cbd6b6651
Yoan Lecoq <yoanlecoq.io@gmail.com>
2018-01-03 15:22:37 +0100

    Lay foundations for FSM. cc #21, #20, #19 and #18

---
+++ Assets/Scripts/Se/Fsm.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Se {
+	public abstract class FsmState {
+		public abstract FsmState OnUpdate(GameObject go);
+		public void OnEnterState(GameObject go) {}
+		public void OnLeaveState(GameObject go) {}
+	}
+
+	public class Fsm {
+		Fsm () {}
+		public Fsm(FsmState initialState) {
+			currentState = initialState;
+		}
+
+		FsmState currentState;
+
+		public void OnStart (GameObject gameObject) {
+			currentState.OnEnterState(gameObject);
+		}
+		public void OnUpdate (GameObject gameObject) {
+			var nextState = currentState.OnUpdate(gameObject);
+			if(nextState == currentState)
+				return;
+			currentState.OnLeaveState(gameObject);
+			nextState.OnEnterState(gameObject);
+			currentState = nextState;
+		}
+	}
+}
---
+++ Assets/Scripts/Se/Hero.cs
@@ -0,0 +1,30 @@
+﻿// NOTE: This is a placeholder for the hero component.
+
+using UnityEngine;
+using System.Collections;
+using Utils;
+
+namespace Se {
+	public class Hero : MonoBehaviour {
+		Fsm fsm = new Fsm(new HeroStates.Idle());
+		void Start () {
+			fsm.AssertNotNull ();
+			fsm.OnStart (gameObject);
+		}
+		void Update() {
+			fsm.OnUpdate (gameObject);
+		}
+	}
+	namespace HeroStates {
+		public class Idle: FsmState {
+			public override FsmState OnUpdate (GameObject go) {
+				throw new System.NotImplementedException ();
+			}
+		}
+		public class Walk: FsmState {
+			public override FsmState OnUpdate (GameObject go) {
+				throw new System.NotImplementedException ();
+			}
+		}
+	}
+}
