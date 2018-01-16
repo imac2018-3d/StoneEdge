ffb16060874e384d5d3f1f493ef70bf97bd72c86
firegreen <steeve.v91@gmail.com>
2018-01-16 23:37:49 +0100

    Add: Transition script, fade in/out and callback (imac2018-3d/StoneEdge#25) Add: NonMap script to detect out of map Modify: PlayerController, add warp function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public float FallVelocityThreshold = -10.0f;
+	public Canvas TransitionCanvas = null;
+
 	private Rigidbody rb;
-	private Vector3 newPos;
+	private Vector3 lastValidPos;
 
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		lastValidPos = rb.transform.position;
+	}
+
+	void Warp()
+	{
+		TransitionCanvas.GetComponent<Transition>().close(
+			() =>
+			{
+				rb.velocity = Vector3.zero;
+				transform.position = lastValidPos;
+				transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+				TransitionCanvas.GetComponent<Transition>().open();
+			}
+		);
+	}
+
+	void CheckFall()
+	{
+		if (rb.velocity.y <= FallVelocityThreshold)
+		{
+			RaycastHit hitInfo;
+			if (!Physics.Raycast(rb.transform.position, rb.velocity, out hitInfo))
+			{
+				Warp();
+			}
+		}
+	}
+
+	void CheckLocation()
+	{
+		RaycastHit hitInfo;
+		if (Physics.Linecast(rb.transform.position, rb.transform.position+rb.velocity*Time.deltaTime*5, out hitInfo))
+		{
+			if (hitInfo.transform.GetComponent<NonMap>() != null)
+			{
+				Warp();
+			}
+		}
 	}
 
 	void FixedUpdate() {
-		rb.constraints = RigidbodyConstraints.FreezeRotation;
-
+		CheckFall();
+		CheckLocation();
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-
+		RaycastHit hitInfo;
 		Vector3 movement = new Vector3 (moveHorizontal/2.0f, 0.0f, moveVertical/2.0f);
-
+		if (Physics.Raycast(rb.transform.position+rb.velocity, -Vector3.up, out hitInfo))
+		{
+			if(hitInfo.transform.GetComponent<NonMap>() == null &&
+				Vector3.Dot(Vector3.up, rb.transform.up) > 0.8f && 
+					(hitInfo.point - transform.position).sqrMagnitude < 1.0f)
+				lastValidPos = rb.transform.position;
+		}
 		rb.MovePosition (transform.position + movement);
 	}
 }
---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+	public float time = 2.0f;
+	public delegate void TransitionCallBack();
+
+	private TransitionCallBack transitionEndCallback = null;
+
+	private CanvasGroup canvasElement;
+	private Coroutine currentCoroutine = null;
+
+	bool isClosed;
+	float currentTarget=-1;
+
+	public void close(TransitionCallBack endCallback = null, float time = -1)
+	{
+		if (isClosed || currentTarget==1)
+			return;
+		if (time < 0)
+			time = this.time;
+		if (currentCoroutine != null)
+			StopCoroutine(currentCoroutine);
+		transitionEndCallback = endCallback;
+		currentCoroutine = StartCoroutine(FadeCanvasGroup(canvasElement, canvasElement.alpha, 1, time));
+	}
+	public void open(TransitionCallBack endCallback = null, float time = -1)
+	{
+		if (!isClosed || currentTarget==0)
+			return;
+		if (time < 0)
+			time = this.time;
+		if (currentCoroutine != null)
+			StopCoroutine(currentCoroutine);
+		transitionEndCallback = endCallback;
+		StartCoroutine(FadeCanvasGroup(canvasElement, canvasElement.alpha, 0, time));
+	}
+
+	public void animate()
+	{
+		if (isClosed)
+			open();
+		else
+			close();
+	}
+
+	public void Awake()
+	{
+		canvasElement = GetComponentInChildren<CanvasGroup>();
+		open(null,0.01f);
+	}
+
+	public bool IsRunning()
+	{
+		return currentCoroutine != null;
+	}
+
+	// Use this for initialization
+	public IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end, float time = 0.5f)
+	{
+		float velocity = 0.05f;
+
+		transform.hideFlags = HideFlags.None;
+		cg.alpha = start;
+		currentTarget = end;
+		while (true)
+		{
+			time -= Time.smoothDeltaTime;
+			if (time <= 0) {
+				cg.alpha = end;
+				break;
+			}
+
+			cg.alpha = Mathf.SmoothDamp(cg.alpha, end, ref velocity, time, Mathf.Infinity, Time.smoothDeltaTime);
+
+			yield return new WaitForEndOfFrame();
+		}
+		if (cg.alpha <= 0)
+		{
+			transform.hideFlags = HideFlags.HideInHierarchy;
+			isClosed = false;
+		}
+		else
+			isClosed = true;
+		currentCoroutine = null;
+		currentTarget = -1;
+		if (transitionEndCallback != null)
+			transitionEndCallback();
+		transitionEndCallback = null;
+	}
+}
