262313e4f0ceec1970b06c3345578fac32cb96b3
CoralieGold <coralie.gold@gmail.com>
2018-01-25 17:42:58 +0100

    First version UI #15

---
+++ Assets/Scripts/Se/GameDatas/GameSaveData.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+
+namespace Se {
+
+	[System.Serializable]
+	public class GameSaveData {
+
+		public int LastCheckpoint = 0;
+		public int[] LoreItems = new int[0];
+		public int[] PowerUps = new int[0];
+
+		private static GameSaveData _instance;
+		public static GameSaveData Instance {
+			get {
+				if (_instance == null) {
+					Se.GameSaveData.LoadGame();
+				}	
+				return _instance;
+			}
+		}
+
+		// NOTE: Saves datas in the gameDatas json file
+		public static void Save() {
+			string json =  Application.dataPath + "/gameDatas.json";
+			string str = JsonUtility.ToJson(_instance);
+			File.WriteAllText(json, str);
+		}
+
+		// NOTE: Load datas from the gameDatas json file
+		public static void LoadGame() {
+			string jsn = File.ReadAllText (Application.dataPath + "/gameDatas.json");
+			_instance = JsonUtility.FromJson<GameSaveData>(jsn);
+			if (Se.GameSaveData.IsEmpty()) { NewGame (); }
+		}
+
+		// NOTE: Checks if the game is new or an saved game
+		public static bool IsEmpty() {
+			if (Instance.LastCheckpoint == 0 && Instance.LoreItems == null && Instance.PowerUps == null) { return true; }
+			else { return false; }
+		}
+
+		// NOTE: Creates a new instance of GameSaveData and save new values
+		public static void NewGame() {
+			_instance = new GameSaveData();
+			Se.GameSaveData.Save();
+		}
+
+		public static int[] GetLoreItems() {
+			return Instance.LoreItems;
+		}
+
+		public static int[] GetPowerUps() {
+			return Instance.PowerUps;
+		}
+
+		public static int GetLastCheckpoint() {
+			return Instance.LastCheckpoint;
+		}
+	}
+}
---
+++ Assets/Scripts/Se/GameDatas/LoreItem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Se {
+	public class LoreItem {
+		public int Type = 0;
+		public string Text = "default";
+
+		public LoreItem(int type, string text) {
+			this.Type = type;
+			this.Text = text;
+		}
+	}
+
+	public static class LoreItemManager {
+		public static Dictionary<int, LoreItem> Lores = new Dictionary<int, LoreItem>()
+	    {
+			{0, new LoreItem(0, "Ohoki's letter")},
+	     	{1, new LoreItem(1, "Astone Mechanics")},
+	     	{2, new LoreItem(2, "Report of Haaris")},
+	     	{3, new LoreItem(3, "Log of Oli Maroon")},
+	     	{4, new LoreItem(3, "Log of Karls")},
+	     	{5, new LoreItem(0, "Jaron's letter")}
+	    };
+	}
+
+}
