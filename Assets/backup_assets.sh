f94b9caf783015c69e251059f277fbe7c323048b
Yoan Lecoq <yoanlecoq.io@gmail.com>
2018-01-25 13:38:32 +0100

    Remove legacy bash scripts (See #37)

--- Assets/backup_assets.sh
+++
@@ -1,8 +0,0 @@
-for i in `find Textures Models Sounds Music -type f`; do
-    if [[ $i == *.meta ]] || [[ $i == *.git* ]]; then
-        continue
-    fi
-    o="LocalBackups/$i"
-    mkdir -p `dirname $o` && cp --update --verbose $i $o
-done
-
