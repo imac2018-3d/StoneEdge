db2539f3329d6bbdf9cd8c1ec7714925d0a99ff1
Yoan Lecoq <yoanlecoq.io@gmail.com>
2018-01-26 16:46:37 +0100

    Improve art.sh by allowing extra arguments

--- Assets/art.sh
+++ Assets/art.sh
@@ -1,11 +1,15 @@
 folder_id=1ByU_jMm3NDlYJfOJXAb3LZZuuYq186Dy
+gdrive_args=${@:2}
 
 case $1 in
+    help)
+        gdrive help sync download $gdrive_args
+        ;;
     pull)
-        gdrive sync download $folder_id Art
+        gdrive sync download $folder_id Art $gdrive_args
         ;;
     push)
-        gdrive sync upload Art $folder_id
+        gdrive sync upload Art $folder_id $gdrive_args
         ;;
     *)
         echo "Usage: $0 <pull|push>"
