c6361b8a8d75a294cac19f3d36ada4084f43db05
CoralieGold <coralie.gold@gmail.com>
2018-02-26 17:41:20 +0100

    Add Loading Screen (#15)

---
+++ Assets/Scripts/Se/HUD/LoadingScreenManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Se {
+
+	public class LoadingScreenManager : MonoBehaviour {
+
+		public GameObject LoadingScreenObj;
+		public GameObject ImageRotate;
+
+		AsyncOperation async;
+
+		// Use this for initialization
+		public void LoadingScreeenManager (int scene) {
+			StartCoroutine (LoadingScreen (scene));
+		}
+		
+		IEnumerator LoadingScreen(int scene) {
+			LoadingScreenObj.SetActive (true);
+			async = SceneManager.LoadSceneAsync (scene);
+			async.allowSceneActivation = false;
+			while (async.isDone == false) {
+				ImageRotate.transform.Rotate (0, Time.deltaTime * 10, 0);
+				if (async.progress == 0.9f) {
+					async.allowSceneActivation = true;
+				}
+				yield return null;
+			}
+		}
+	}
+}
