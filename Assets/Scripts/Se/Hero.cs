287ec933528686fd7ec6da335b52f6b38e3e4d8b
Yoan Lecoq <yoanlecoq.io@gmail.com>
2018-03-11 13:56:03 +0100

    More or less implement punching and kicking

--- Assets/Scripts/Se/Hero.cs
+++ Assets/Scripts/Se/Hero.cs
@@ -14,10 +14,14 @@ namespace Se {
 		public float AirMovementSpeedFactor = 0.5f;
 		public float JumpStrength = 80.0f;
 		public float FallSpeedFactor = 3f;
+		public float DodgeStrength = 25f;
+		public float DodgeDuration = 1f;
+		public float PunchColliderDuration = 0.4f;
+		public float PunchCooldownDuration = 1f;
 		public int MaxLife = 5;
 
 		// private or hidden attributes
-		Fsm fsm = new Fsm(new HeroStates.Idle());
+		Fsm fsm = new Fsm(new HeroStates.Movable());
 		internal int life;
 		internal Vector3 moveDirection = Vector3.zero;
 
@@ -52,18 +56,46 @@ namespace Se {
 			forward.Normalize ();
 			return right * input.x + forward * input.y;
 		}
+			
+		public void KeepAirKicking() {
+			KeepPunching ();
+		}
+		public void KeepPunching() {
+			if (canPunch) {
+				doPunch ();
+			}
+		}
+
+		float lastPunchStartTime = 0f;
+		bool canPunch { get { return lastPunchStartTime==0f ? true : Time.time - lastPunchStartTime > PunchCooldownDuration; } }
+		public void doPunch() {
+			lastPunchStartTime = Time.time;
+			var go = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+			go.transform.SetParent (transform, false);
+			go.transform.Translate (Vector3.forward, Space.Self);
+			go.transform.Rotate(Vector3.right, 90f, Space.Self);
+			go.AddComponent<ElectrickPunching> ();
+			Destroy (go, PunchColliderDuration);
+		}
 	}
 
 	namespace HeroStates {
-		public class Idle: FsmState {
+		public class Movable: FsmState {
 			public override FsmState OnUpdate (GameObject go) {
 				var hero = go.GetComponent <Hero>();
 				var ctrl = go.GetComponent <CharacterController> ();
+
 				if (ctrl.isGrounded) {
+					if (InputActions.Dodges)
+						return new Dodge ();
+					if (InputActions.IsPunching)
+						hero.KeepPunching();
 					hero.moveDirection = hero.GetMovementInput () * hero.GroundMovementSpeedFactor;
 					if (InputActions.Jumps)
 						hero.moveDirection.y = hero.JumpStrength;
 				} else {
+					if (InputActions.IsAirKicking)
+						hero.KeepAirKicking();
 					var v = hero.GetMovementInput () * hero.AirMovementSpeedFactor;
 					var candidate = hero.moveDirection + v;
 					candidate.y = 0f;
@@ -80,99 +112,21 @@ namespace Se {
 			}
 		}
 
-		public class Walk: FsmState {
-			private Vector3 newPos;
-			private float timeForMagnetic;
-
-			public override FsmState OnUpdate (GameObject go){
-				/*
-				if (go.GetComponent<Hero> ().grounded && Input.GetKeyDown (KeyCode.Space)) {
-					return new Jump ();
-				}else if (go.GetComponent<Hero> ().grounded && Input.GetKeyDown (KeyCode.X)) {
-					return new Punch ();
-				}else if (go.GetComponent<Hero> ().grounded && Input.GetKeyDown (KeyCode.C)) {
-					return new MagnetImpact ();
-				}else if (InputActions.Dodges) {
-					return new Dodge ();
-				}
-				*/
-				return this;
-			}
-
-			public override FsmState OnFixedUpdate (GameObject go) {
-				/*
-				float moveHorizontal = Input.GetAxis ("Horizontal");  // Get the horizontal keyboard arrows "forces"
-				float moveVertical = Input.GetAxis ("Vertical");  // Get the vertical keyboard arrows "forces"
-
-				Vector3 velocity = go.GetComponent<Hero> ().rb.velocity;
-				Vector3 movement = new Vector3 (moveHorizontal * go.GetComponent<Hero> ().speed, velocity.y, moveVertical * go.GetComponent<Hero> ().speed);  // Create a vector to move the character 
-
-				go.GetComponent<Hero>().rb.velocity = movement;  // Move the character
-
-				Vector3 forward = go.GetComponent<Hero> ().rb.transform.forward;
-				forward = forward.normalized;
-				forward.y = 0f;
-
-				Vector3 directionTo = movement.normalized ;
-				directionTo = directionTo.normalized;
-				directionTo.y = 0f;
-
-				Vector3 between = Vector3.Lerp (forward, directionTo, 0.2f);
-				//Debug.Log ("Between : "+ between.x + ", " + between.y + ", " + between.z);
-				Quaternion reorientation = Quaternion.LookRotation (between, forward);
-				//reorientation.Set(reorientation.x, reorientation.y, 0f, reorientation.z);
-
-				//go.GetComponent<Hero> ().rb.rotation = reorientation;
-				*/
-				return this;
-			}
-		}
-
-		public class Jump: FsmState {
-			private Vector3 jumpMovement;
-
-			public override FsmState OnUpdate (GameObject go) {
-				/*
-				jumpMovement = new Vector3 (0.0f, go.GetComponent<Hero> ().jumpValue, 0.0f);
-				go.GetComponent<Hero>().rb.AddForce(jumpMovement, ForceMode.Impulse);
-				*/
-				return new Idle ();
-			}
-		}
-
-		public class MagnetImpact: FsmState {
-			public override FsmState OnUpdate (GameObject go) {
-				/*
-				go.GetComponent<Hero> ().playerEnabledToMoveMagnetImpact = false;
-				go.GetComponent<Hero>().magnetImpactGo.SetActive (true);
-				*/
-				return new Idle();
-			}
-		}
-
-		public class Punch: FsmState {
-			public override FsmState OnUpdate (GameObject go) {
-				/*
-				go.GetComponent<Hero> ().playerEnabledToMovePunch = false;
-				go.GetComponent<Hero>().punchGo.SetActive (true);
-				*/
-				return new Idle();
-			}
-		}
-
 		public class Dodge: FsmState {
-			public override FsmState OnFixedUpdate (GameObject go) {
-				/*
-				float moveHorizontal = Input.GetAxis ("Horizontal");  // Get the horizontal keyboard arrows "forces"
-				float moveVertical = Input.GetAxis ("Vertical");  // Get the vertical keyboard arrows "forces"
-
-				Vector3 velocity = go.GetComponent<Hero> ().rb.velocity;
-				//Vector3 movement = new Vector3 (moveHorizontal * go.GetComponent<Hero> ().speed * 20, velocity.y, moveVertical * go.GetComponent<Hero> ().speed * 20);  // Create a vector to move the character 
-				Vector3 movement = new Vector3 (moveHorizontal * go.GetComponent<Hero> ().speed * 20, velocity.y, moveVertical * go.GetComponent<Hero> ().speed * 20);  // Create a vector to move the character 
-
-				go.GetComponent<Hero>().rb.velocity = movement;  // Move the character
-				*/
-				return new Idle();
+			float startTime;
+			public override void OnEnterState (GameObject go) {
+				var hero = go.GetComponent <Hero>();
+				hero.moveDirection = go.transform.forward * hero.DodgeStrength;
+				startTime = Time.time;
+			}
+			public override FsmState OnUpdate (GameObject go) {
+				var hero = go.GetComponent <Hero>();
+				var ctrl = go.GetComponent <CharacterController> ();
+				hero.moveDirection = Vector3.Lerp (hero.moveDirection, Vector3.zero, (Time.time - startTime) / hero.DodgeDuration);
+				ctrl.Move(hero.moveDirection * Time.deltaTime);
+				if (hero.moveDirection.magnitude <= 0.5f)
+					return new Movable ();
+				return this;
 			}
 		}
 	}
