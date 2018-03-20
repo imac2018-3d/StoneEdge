bfb4d31037358011c4dad4c05ac6a86ed89e8f98
Lancel Thaledric <thaledric@gmail.com>
2018-03-20 16:21:35 +0100

    script enenmy avec fsm en cours de construction

--- Assets/Scripts/Se/Enemy.cs
+++ Assets/Scripts/Se/Enemy.cs
@@ -16,7 +16,15 @@ namespace Se {
 		public static int lifeMax = 3;
 		public int life = lifeMax;
 
-		void Start () {
+        public Transform Target;
+        public float maxDistanceSight = 5.0f;
+
+        public float timeBetweenAttacks = 2.0f;
+        private float lastAttackTimeElapsed = 0.0f;
+        public bool timeToAttack () { return lastAttackTimeElapsed - Time.time > timeBetweenAttacks; }
+        public void resetAttackTimer() { lastAttackTimeElapsed = Time.time; }
+
+        void Start () {
 			rb = GetComponent<Rigidbody> ();
 			fsm.AssertNotNull ();
 			fsm.OnStart (gameObject);
@@ -38,13 +46,54 @@ namespace Se {
 	namespace EnemyStates {
 		public class Idle: FsmState {
 			public override FsmState OnUpdate (GameObject go) {
-				return this;
+                var enemy = go.GetComponent<Enemy>();
+                // TODO check if target is in sight
+                if (! Physics.Raycast(go.transform.position, enemy.Target.position - go.transform.position)) // hero is visible
+                {
+                    if ((enemy.Target.position - go.transform.position).magnitude < enemy.maxDistanceSight) // hero is close enough
+                    {
+                        if(enemy.timeToAttack()) // it's time to attack
+                        {
+                            return new Attack();
+                        } else // it's not time to attack, wait more
+                        {
+                            return this;
+                        }
+                        
+                    }
+                }
+                return this;
 			}
-		}
+
+            // is this needed ?
+            public override void OnEnterState(GameObject go)
+            {
+                // TODO
+            }
+            public override void OnLeaveState(GameObject go)
+            {
+                // TODO
+            }
+        }
 		public class Walk: FsmState {
 			public override FsmState OnUpdate (GameObject go) {
 				return this;
 			}
 		}
+        public class Attack: FsmState {
+            public override FsmState OnUpdate(GameObject go)
+            {
+                return this;
+            }
+            public override void OnEnterState(GameObject go)
+            {
+                var enemy = go.GetComponent<Enemy>();
+                enemy.resetAttackTimer();
+            }
+            public override void OnLeaveState(GameObject go)
+            {
+                // TODO
+            }
+        }
 	}
 }
