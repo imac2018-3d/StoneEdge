14340f60b35ede3f756739167e89a3da35cb0896
firegreen <steeve.v91@gmail.com>
2018-03-27 19:30:35 +0200

    :clap:

--- Assets/Scripts/Se/Enemy.cs
+++ Assets/Scripts/Se/Enemy.cs
@@ -1,99 +1,173 @@
-﻿// NOTE: This is a placeholder for the Enemy component.
-
-using UnityEngine;
+﻿using System;
 using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
 using Utils;
 
-namespace Se {
-	public class Enemy : MonoBehaviour {
-		Fsm fsm = new Fsm(new EnemyStates.Idle());
+namespace Se
+{
 
-		public Rigidbody rb;
+	[RequireComponent(typeof(CharacterController))]
+	public class Enemy : MonoBehaviour
+	{
+		// public attributes, tweakable in the inspector
+		public float GroundMovementSpeedFactor = 15f;
+		public int MaxLife = 5;
+		public float MaxDistFromInitialPosition = 3;
+		public float IdleIntervalDuration = 5;
+		public float MaxDistToHeroToAttack = 20;
+		public float MaxDistFromInitialPosWhileAttacking = 40;
 
-		public float fallMultiplier = 3.5f; // the multiplier to make the falling down part quicker than the jumping part
-		public float lowJumpMultiplier = 3f; // The multiplier to make the jumping part lower than the fall down
+		// private or hidden attributes
+		Fsm fsm = new Fsm(new EnemyStates.Movable());
+		internal int life;
+		internal Vector3 moveDirection = Vector3.zero;
+		internal Animator animator;
+		internal Vector3 initialPosition;
+		internal Vector3 currentDestination;
+		internal float lastIdleDestinationChange;
+		internal Hero hero;
 
-		public static int lifeMax = 3;
-		public int life = lifeMax;
-
-        public Transform Target;
-        public float maxDistanceSight = 5.0f;
-
-        public float timeBetweenAttacks = 2.0f;
-        private float lastAttackTimeElapsed = 0.0f;
-        public bool timeToAttack () { return lastAttackTimeElapsed - Time.time > timeBetweenAttacks; }
-        public void resetAttackTimer() { lastAttackTimeElapsed = Time.time; }
-
-        void Start () {
-			rb = GetComponent<Rigidbody> ();
-			fsm.AssertNotNull ();
-			fsm.OnStart (gameObject);
+		internal Vector3 randomDestinationInIdleCircle()
+		{
+			Vector2 randomPoint = UnityEngine.Random.insideUnitCircle;
+			return initialPosition + MaxDistFromInitialPosition * new Vector3(randomPoint.x, 0, randomPoint.y);
 		}
-		void Update() {
-			if(rb.velocity.y < 0) {
-				rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier) * Time.deltaTime;
-			} else if (rb.velocity.y > 0 && !Input.GetKeyDown (KeyCode.Space)) {
-				rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier) * (Time.deltaTime);
+
+		void Start()
+		{
+			Assert.IsTrue(MaxLife > 0);
+			life = MaxLife;
+			initialPosition = transform.position;
+			lastIdleDestinationChange = Time.time;
+			hero = FindObjectOfType<Hero>();
+			Assert.IsNotNull(hero, "Pas de héros dans le scene!!!!!!!!!!!!!!!!");
+			animator = GetComponentInChildren<Animator>();
+			fsm.OnStart(gameObject);
+		}
+		void Update()
+		{
+			fsm.OnUpdate(gameObject);
+		}
+		void FixedUpdate()
+		{
+			fsm.OnFixedUpdate(gameObject);
+		}
+
+		public void ReceiveDamage(int amount)
+		{
+			life -= amount;
+			if (life <= 0)
+			{
+				life = 0;
+				// NOTE TODO: If we're here, the enemy died.
 			}
-
-			fsm.OnUpdate (gameObject);
 		}
 
-		void looseLife() {
-			life--;
+		internal bool seeHeroCloseEnough()
+		{
+			// if close enough
+			if((hero.transform.position - transform.position).magnitude > MaxDistToHeroToAttack)
+			{
+				return false;
+			}
+			// if hero visible
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, hero.transform.position - transform.position, out hit))
+			{
+				if(hit.collider.gameObject.GetComponent<Hero>() != null)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		internal bool arrivedToDestination()
+		{
+			return (currentDestination - transform.position).sqrMagnitude < 0.01;
+		}
+
+		public Vector3 GetMovementInput()
+		{
+			if (!arrivedToDestination())
+			{
+				return (currentDestination - transform.position).normalized;
+			}
+			else
+			{
+				return Vector3.zero;
+			}
 		}
 	}
-	namespace EnemyStates {
-		public class Idle: FsmState {
-			public override FsmState OnUpdate (GameObject go) {
-                var enemy = go.GetComponent<Enemy>();
-                // TODO check if target is in sight
-                if (! Physics.Raycast(go.transform.position, enemy.Target.position - go.transform.position)) // hero is visible
-                {
-                    if ((enemy.Target.position - go.transform.position).magnitude < enemy.maxDistanceSight) // hero is close enough
-                    {
-                        if(enemy.timeToAttack()) // it's time to attack
-                        {
-                            return new Attack();
-                        } else // it's not time to attack, wait more
-                        {
-                            return this;
-                        }
-                        
-                    }
-                }
-                return this;
-			}
 
-            // is this needed ?
-            public override void OnEnterState(GameObject go)
-            {
-                // TODO
-            }
-            public override void OnLeaveState(GameObject go)
-            {
-                // TODO
-            }
-        }
-		public class Walk: FsmState {
-			public override FsmState OnUpdate (GameObject go) {
+	namespace EnemyStates
+	{
+		public class Movable : FsmState
+		{
+			public override FsmState OnUpdate(GameObject go)
+			{
+				var enemy = go.GetComponent<Enemy>();
+				var ctrl = go.GetComponent<CharacterController>();
+
+				// update idle walk destination
+				if(Time.time - enemy.lastIdleDestinationChange > enemy.IdleIntervalDuration
+					|| enemy.arrivedToDestination())
+				{
+					enemy.currentDestination = enemy.randomDestinationInIdleCircle();
+					enemy.lastIdleDestinationChange = Time.time;
+				}
+
+				// test if enemy has to attack hero
+				if (enemy.seeHeroCloseEnough())
+				{
+					return new Attacking();
+				}
+
+				if (ctrl.isGrounded)
+				{
+					enemy.moveDirection = enemy.GetMovementInput() * enemy.GroundMovementSpeedFactor;
+				}
+				enemy.moveDirection += Physics.gravity * Time.deltaTime;
+				ctrl.Move(enemy.moveDirection * Time.deltaTime);
+
+				var dir = enemy.moveDirection;
+				dir.y = 0f;
+				go.transform.LookAt(go.transform.position + dir);
+
 				return this;
 			}
 		}
-        public class Attack: FsmState {
-            public override FsmState OnUpdate(GameObject go)
-            {
-                return this;
-            }
-            public override void OnEnterState(GameObject go)
-            {
-                var enemy = go.GetComponent<Enemy>();
-                enemy.resetAttackTimer();
-            }
-            public override void OnLeaveState(GameObject go)
-            {
-                // TODO
-            }
-        }
+		public class Attacking : FsmState
+		{
+			public override FsmState OnUpdate(GameObject go)
+			{
+				var enemy = go.GetComponent<Enemy>();
+				var ctrl = go.GetComponent<CharacterController>();
+
+				// update destination
+				enemy.currentDestination = enemy.hero.transform.position;
+
+				// need to give up?
+				if(!enemy.seeHeroCloseEnough() 
+					|| (enemy.transform.position - enemy.initialPosition).magnitude > enemy.MaxDistFromInitialPosWhileAttacking)
+				{
+					return new Movable();
+				}
+
+				if (ctrl.isGrounded)
+				{
+					enemy.moveDirection = enemy.GetMovementInput() * enemy.GroundMovementSpeedFactor;
+				}
+				enemy.moveDirection += Physics.gravity * Time.deltaTime;
+				ctrl.Move(enemy.moveDirection * Time.deltaTime);
+
+				var dir = enemy.moveDirection;
+				dir.y = 0f;
+				go.transform.LookAt(go.transform.position + dir);
+
+				return this;
+			}
+		}
 	}
 }
